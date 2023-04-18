bad4859548daeca88dc847d04af5870d3c153eb0
analebyodkina <dp030992cad@gmail.com>
2023-04-18 12:23:48 +0300

    react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet"></link>
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
