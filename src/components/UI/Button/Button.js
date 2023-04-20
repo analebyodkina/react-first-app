26b5943e15393b90f3b903471fb21e5e324581b9
analebyodkina <dp030992cad@gmail.com>
2023-04-20 16:36:19 +0300

    made button a separate component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,7 +2,10 @@ import "./Button.scss";
 
 function Button(props) {
   return (
-    <></>
+    <button className={`btn ${props.addClass || ""}`} type={props.type}>
+      <span>{props.icon}</span>
+      <span className="btn__text">{props.title}</span>      
+    </button>
   );
 }
 
